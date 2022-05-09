eee52bf5de2435c5b8056b98772fcd62f3e632cd
wesleyKaihara <wesleykaihara@gmail.com>
2022-05-09 17:40:29 -0300

    Icones

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,16 +1,18 @@
 import style from './style.module.scss';
 import Logo from '../../images/logo-default.png'
 
+import HomeIcon from '@mui/icons-material/Home';
+import ContactsIcon from '@mui/icons-material/Contacts';
 export default function Header() {
   return (
     <header className={style.header}>
       <img src={Logo} alt="Logo_Empresa" className={style.Logo} />
       <nav className={style.navMenu}>
         <ul>
-          <li><a href='/' >Home</a></li>
+          <li><a href='/' ><HomeIcon /> Home</a></li>
           <li><a href='/Catalogo' >Catálogo</a></li>
-          <li><a href='/Contato'>Contato</a></li>
-          <li><a href="/Login">Entrar/Cadastrar</a></li>
+          <li><a href='/Contato'><ContactsIcon /> Contato</a></li>
+          <li className={style.btn}><a href="/Login">Entrar/Cadastrar</a></li>
         </ul>
 
       </nav>
--- src/Pages/Produto/index.tsx
+++ src/Pages/Produto/index.tsx
@@ -4,7 +4,7 @@ import Img from '../../images/defaultItem.png'
 
 import { Button, Modal } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-
+import ArticleIcon from '@mui/icons-material/Article';
 
 
 export default function Produto() {
@@ -43,24 +43,38 @@ export default function Produto() {
       <div className={style.container}>
         <div className={style.produtos_img}>
           <img src={Img} alt="Produto" className={style.img} />
+          <img src={Img} alt="Produto" className={style.avaliacao} />
         </div>
         <div className={style.produtos_info}>
           <h1 className={style.produto_name}>Nome Produto</h1>
-          <input
-            type="number"
-            name="quantidade"
-            id="quantidade"
-            min={1}
-            defaultValue={1}
-            className={style.quantidade} />
-          <Button variant="primary" onClick={handleShow}>
-            Calcular Frete
-          </Button>
+
+          <h3 className={style.valorAntigo}>R$1200,00</h3>
+          <h2 className={style.valorNovo}>R$999,99</h2>
+          <div className={style.quantContainer}>
+            <label htmlFor="quantidade">Quantidade</label>
+            <input
+              type="number"
+              name="quantidade"
+              id="quantidade"
+              min={1}
+              defaultValue={1}
+              className={style.quantidade} /><span>Unid</span>
+            <button className={style.btn}>Adicionar ao Carrinho</button>
+          </div>
+
+          <div className={style.freteContainer}>
+            <button className={style.btn} onClick={handleShow}>Calcular Frete</button>
+          </div>
+          <div className={style.produtos_desc}>
+            <h1><ArticleIcon />Descrição do Produto</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni natus corporis officia voluptatibus voluptatem cupiditate, quibusdam cumque eius quasi, quisquam aspernatur? Delectus quas excepturi facere necessitatibus modi. Atque, quisquam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni natus corporis officia voluptatibus voluptatem cupiditate, quibusdam cumque eius quasi, quisquam aspernatur? Delectus quas excepturi facere necessitatibus modi. Atque, quisquam.</p>
+          </div>
         </div>
       </div>
 
       {/* Modal */}
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} >
         <Modal.Header closeButton>
           <Modal.Title>Calculo de Frete</Modal.Title>
         </Modal.Header>
